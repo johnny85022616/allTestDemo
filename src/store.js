d611705e5d86cfe6b9a522425308844c4373b600
Johnny <yu.chang@infopowercorp.com>
2020-07-06 18:42:13 +0800

    表單頁面

--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
-import {createStore , compose} from 'redux';
+import {createStore , compose , combineReducers} from 'redux';
 import {counterReducer} from './reducers/counterReducer.js'
+import {FormReducer} from './reducers/formResucer.js';
 
+//conbine reducers
+const allReducer = combineReducers({
+    counterReducer,
+    FormReducer
+    })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(counterReducer , composeEnhancer());
+export const store = createStore(allReducer , composeEnhancer());
