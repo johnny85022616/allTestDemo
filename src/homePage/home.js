0738974ee1df84052c415809f0bb97737c8cec73
張裕 <nte.82002@yahoo.com.tw>
2020-07-09 01:45:48 +0800

    首頁button action

--- src/homePage/home.js
+++ src/homePage/home.js
@@ -19,28 +19,58 @@ const HomeTitle = styled.div`
     justify-content:center;
 `;
 
-const ActionButton = styled.button`
-height:40px;
-width:100px;
-background-color:gray;
-color:white;
-display:flex;
-item-align:center;
-justify-content:center;
-margin:20px auto;
+const ButtonCss = (
+    `height:40px;
+    width:100px;
+    background-color:gray;
+    color:white;
+    display:flex;
+    item-align:center;
+    justify-content:center;
+    margin:20px auto;`
+)
+
+const IncreaseButton = styled.button`
+${ButtonCss}
+${({action})=>{
+    if(action=='1'){
+        return `background-color:Blue;`
+    }
+}}
 `;
 
+const DeleteButton = styled.button`
+${ButtonCss}
+${({action})=>{
+    if(action=='2'){
+        return `background-color:Blue;`
+    }
+}}
+`;
 
+const ChangeButton = styled.button`
+${ButtonCss}
+${({action})=>{
+    if(action=='3'){
+        return `background-color:Blue;`
+    }
+}}
+`;
+  
+  export const Home = ()=>{
+      const [action,setAction] = useState('0');
+      
+      const handleClick = (i)=>{
+          setAction(i)
+      }
 
-export const Home = ()=>{
-    
   return (
     <HomeBlock>
         <HomeTitle>新刪修首頁</HomeTitle>
         <ButtonBlock>
-            <ActionButton>新增</ActionButton>
-            <ActionButton>刪除</ActionButton>
-            <ActionButton>修改</ActionButton>
+            <IncreaseButton key={'1'} action={action} onClick={()=>{handleClick('1')}}>新增</IncreaseButton>
+            <DeleteButton key={'2'} action={action} onClick={()=>{handleClick('2')}}>刪除</DeleteButton>
+            <ChangeButton key={'3'} action={action} onClick={()=>{handleClick('3')}}>修改</ChangeButton>
         </ButtonBlock>
         <IncreaseForm/>
     </HomeBlock>
